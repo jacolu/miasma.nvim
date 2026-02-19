9ceeb210c0c327bb11845e3dc562247d0f012fe3
jacolu <guy@jacolu.dev>
2026-02-19 13:39:22 -0500

    init commit

---
+++ lua/miasma/init.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.setup = function()
+  local groups = require("miasma.groups").setup()
+  for group, setting in pairs(groups) do
+    vim.api.nvim_set_hl(0, group, setting)
+  end
+end
+return M
