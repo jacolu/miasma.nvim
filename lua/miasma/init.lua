2fa533c69459fe293d6cd39130f7fa5f4d2d06cc
jacolu <guy@jacolu.dev>
2026-02-19 13:54:46 -0500

    fix: report color name

--- lua/miasma/init.lua
+++ lua/miasma/init.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.setup = function()
   local groups = require("miasma.groups").setup()
+  vim.g.colors_name = "miasma"
   for group, setting in pairs(groups) do
     vim.api.nvim_set_hl(0, group, setting)
   end
